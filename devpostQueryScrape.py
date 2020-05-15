fd67d361af748d82c6d8100f90065e7d03a25db5
lmcintosh623 <lmcintosh623@gmail.com>
2020-05-14 23:07:34 -0700

    added intial file with commments, need to implement code.

---
+++ devpostQueryScrape.py
@@ -0,0 +1,5 @@
+#   URL: https://devpost.com/hackathons?utf8=%E2%9C%93&search=INSERTSEARCHHERE&challenge_type=all&sort_by=Recently+Added
+#
+#   Sanitize the input (elements of the list from hackathonNamess.txt) and inject into INSERTSEARCHHERE
+#       -write conditional to check date posted? 
+#       -start scraping github links
