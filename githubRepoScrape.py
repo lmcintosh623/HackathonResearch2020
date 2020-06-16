5f8a08132508662cc663d37cc245fe61b7e8178d
lmcintosh623 <lmcintosh623@gmail.com>
2020-06-15 18:44:39 -0700

    This script was used to iterate through the project pages in listOfValidHackathonURLs.txt and scrape all of the accosiated links for a given project

---
+++ githubRepoScrape.py
@@ -0,0 +1,33 @@
+import requests
+import sys
+import re
+from bs4 import BeautifulSoup
+
+res = open("projectLinksScraped.txt", "w")
+
+def individualProject(argument):
+    reg = "<a target=\"_blank\".*href=\"(.*?)\""
+    project = requests.get(str(argument.strip())).text
+    links = re.findall(reg, str(project))
+    for i in range(len(links)):
+        res.write(links[i] + "\n")
+
+def scrapeProjectLinks(argument):
+    reg = "<a class=\"block-wrapper-link fade link-to-software\" href=\"(.*?)\">"
+    hackathon = requests.get(str(argument.strip())).text
+    results = re.findall(reg, str(hackathon))
+    #print(results)
+    for i in range(len(results)):
+        print("Iteration " + str(i))
+        individualProject(results[i])
+
+
+
+def driverFunction():
+    f = open("listOfValidHackathonURLs.txt", "r") 
+    for line in f.readlines():
+        scrapeProjectLinks(line)
+    f.close()
+    
+driverFunction()
+res.close()
