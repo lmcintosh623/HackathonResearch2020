eced7e19348e65478989727a7a22e447e5638bdc
lmcintosh623 <lmcintosh623@gmail.com>
2020-07-16 15:34:22 -0700

    Updated the github link scrape to exhaustively process the webpage, rather tham the old proccess-24-and-move-on style. Now processing every submission page.

--- githubRepoScrape.py
+++ githubRepoScrape.py
@@ -9,7 +9,7 @@ def individualProject(argument):
     project = requests.get(str(argument.strip())).text
     links = re.findall(reg, str(project))
     for i in range(len(links)):
-        res.write(links[i])
+        res.write(links[i]+'\n')
 
 def scrapeProjectLinks(argument):
     reg = "<a class=\"block-wrapper-link fade link-to-software\" href=\"(.*?)\">"
@@ -21,12 +21,24 @@ def scrapeProjectLinks(argument):
         individualProject(results[i])
 
 
-
 def driverFunction():
-    f = open("listOfValidHackathonURLs.txt", "r") 
+    f = open("listOfValidHackathonURLs.txt", "r")
+    hackathonNum = 0
     for line in f.readlines():
+        hackathonNum += 1
+        print("New URL #" + str(hackathonNum) + ": " + str(line.strip()))
+        index = 2
         scrapeProjectLinks(line)
-    f.close()
+        nextPage = str(line.strip()+"?page="+str(index))
+        print("Next URL: " + nextPage)
+        reg1 = "(There are no submissions which match your criteria.)"
+        reg2 = "(The hackathon managers haven't published this gallery yet, but hang tight!)"
+        while(re.search(reg1, requests.get(nextPage).text) is None and re.search(reg2, requests.get(nextPage).text) is None):
+            print("Success " + str(index) + ": " + nextPage)
+            scrapeProjectLinks(nextPage)
+            index += 1
+            nextPage = str(line.strip()+"?page="+str(index))
+
     
 driverFunction()
 res.close()
